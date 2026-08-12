82945dec991178564dce018209a634c3acbfcc0a
Caio Freitas <cfam@cesar.school>
2026-08-12 17:36:55 -0300

    Atualização Pasta Views

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.urls import path
 from . import views
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Nossa primeira aplicação Django.</h1>")
 
 urlpatterns = [
     path("", views.index),
